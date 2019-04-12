b5d347442972c0474fb7662f6eeebd62ee368cf5
Parviz Abdulloev <parviz23.10@inbox.ru>
2019-04-12 16:31:25 +0300

    change buttons, add class 'AI'

---
+++ TicTacToe 4x4/AI.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe_4x4
+{
+    class AI
+    {
+        public static void MoveAI(List<Button> ListOfButtons)
+        {
+            if (isWin(ListOfButtons))
+                return;
+            //else if (isHumanWin(ListOfButtons))
+            //    return;
+        }
+
+        /// <summary>
+        /// Ход компьютера
+        /// </summary>
+        private static void PerformMove(Button button)
+        {
+            button.Content = "O";
+            button.IsEnabled = false;
+        }
+
+
+        /// <summary>
+        /// Если следующим ходом выиграет компьюьтер
+        /// </summary>
+        private static bool isWin(List<Button> ListOfButtons)
+        {
+            if (isHorizontalWin(ListOfButtons))
+                return true;
+
+
+            return false;
+        }
+
+        private static bool isAi(Button button)
+        {
+            if (button.Content.Equals("O"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+        /// <summary>
+        /// Проверяем каждую строку на наличие победы
+        /// </summary>
+        private static bool isHorizontalWin(List<Button> ListOfButtons)
+        {
+            for (byte i = 0; i < 4; i++)
+            {
+                Button firstButton = ListOfButtons.ElementAt(i * 4);
+                Button secondButton = ListOfButtons.ElementAt((i * 4) + 1);
+                Button thirdButton = ListOfButtons.ElementAt((i * 4) + 2);
+                Button fouthButton = ListOfButtons.ElementAt((i * 4) + 3);
+
+                if (isAi(firstButton) && isAi(secondButton) && isAi(thirdButton))
+                {
+                    if (fouthButton.IsEnabled == true)
+                    {
+                        PerformMove(fouthButton);
+                        return true;
+                    }
+                }
+
+                else if (isAi(firstButton) && isAi(secondButton) && isAi(fouthButton))
+                {
+                    if (thirdButton.IsEnabled == true)
+                    {
+                        PerformMove(thirdButton);
+                        return true;
+                    }
+                }
+
+                else if (isAi(firstButton) && isAi(thirdButton) && isAi(fouthButton))
+                {
+                    if (secondButton.IsEnabled == true)
+                    {
+                        PerformMove(secondButton);
+                        return true;
+                    }
+                }
+
+                else if (isAi(secondButton) && isAi(thirdButton) && isAi(fouthButton))
+                {
+                    if (firstButton.IsEnabled == true)
+                    {
+                        PerformMove(firstButton);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+
+    }
+}
--- TicTacToe 4x4/MainWindow.xaml.cs
+++ TicTacToe 4x4/MainWindow.xaml.cs
@@ -21,59 +21,48 @@ namespace TicTacToe_4x4
     public partial class MainWindow : Window
     {
         private List<Button> ListOfButtons;
-        private List<Button> BEST_MOVES;
 
         public MainWindow()
         {
             InitializeComponent();
 
+            // Список кнопок (полей)
             ListOfButtons = new List<Button>() {
                 A1, A2, A3, A4,
                 B1, B2, B3, B4,
                 C1, C2, C3, C4,
                 D1, D2, D3, D4 };
-
-            BEST_MOVES = new List<Button>() {
-                B2, C3, B3, C2,
-                A1, A4, D1, D4,
-                A2, A3, B1, C1,
-                D2, D3, B4, C4};
         }
 
 
         private void Button_Click(object sender, RoutedEventArgs e)
-        { 
+        {
             var button = (Button)sender;
-            if (ListOfButtons.Contains(button))
+            button.Content = "X";
+            button.IsEnabled = false;
+
+            AI.MoveAI(ListOfButtons);
+
+            CheckWinner();
+        }
+
+        private void Button_Click_Restart(object sender, RoutedEventArgs e)
+        {
+            foreach (var button in ListOfButtons)
             {
-                button.Content = "X";
-                BEST_MOVES.Remove(button);
-                var move = MoveAI(ListOfButtons);
+                button.IsEnabled = true;
+                button.Content = "";
             }
         }
 
-        //private void Button_Click_Restart(object sender, RoutedEventArgs e)
-        //{
-        //    foreach (var element in ListOfButtons)
-        //    {
-        //        element.Content = "";
-        //    }
-        //}
 
-        /// <summary>
-        /// Ход компьютера
-        /// </summary>
-        private Button MoveAI(List<Button> ListOfButtons)
+        private void CheckWinner()
         {
-            Button move = new Button();
-
-            foreach (var button in BEST_MOVES)
+            if ((A1.ContentStringFormat == "X") && (A2.ContentStringFormat == "X") && (A3.ContentStringFormat == "X") && (A4.ContentStringFormat == "X"))
             {
-                button.Content = "O";
-                BEST_MOVES.Remove(button);
-                return move;
+                MessageBox.Show("Поздравляем вы выиграли!");
+                return;
             }
-            return move;
         }
 
 
